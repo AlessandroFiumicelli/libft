d14f5f66b1a984055fa94760c67dcb52c6b278e6
root <root@DESKTOP-MKBD00V.localdomain>
2018-11-18 13:53:56 +0100

    fix ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -11,13 +11,25 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	len;
 
 	len = 0;
+	if (n == 0)
+		return (0);
 	while ((s1[len] || s2[len]) && s1[len] == s2[len] && len < n)
 		len++;
+	if (len == n && (!s1[len] || !s2[len]))
+		return (0);
 	return (*((unsigned char *)s1 + len) - *((unsigned char *)s2 + len));
+}
+int	main(void)
+{
+	printf("%d\n", ft_strncmp("abc", "abcde", 3));
+	printf("%d\n", strncmp("abc", "abcde", 3));
+	printf("%d\n", ft_strncmp("q", "a", 0));
+	printf("%d\n", strncmp("q", "a", 0));
+	return (0);
 }
