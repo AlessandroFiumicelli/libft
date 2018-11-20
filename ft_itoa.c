21348a420f7c3609590aa4c9563ddc3b73728614
Alessandro FIUMICELLI <alfiumic@e3r11p11.42.fr>
2018-11-20 17:19:33 +0100

    add final libft

---
+++ ft_itoa.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alfiumic <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/15 12:08:46 by alfiumic          #+#    #+#             */
+/*   Updated: 2018/11/19 13:08:22 by alfiumic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char		*ft_makestr(char *str, size_t size, long int conv, int neg)
+{
+	size_t	tmpsize;
+
+	tmpsize = size - 1;
+	if (conv == 0)
+		str[0] = '0';
+	else
+	{
+		while (conv > 0)
+		{
+			if (neg)
+				str[0] = '-';
+			str[tmpsize] = (char)((conv % 10) + 48);
+			conv = conv / 10;
+			tmpsize--;
+		}
+	}
+	return (str);
+}
+
+static size_t	get_size(long int n)
+{
+	size_t	size;
+
+	size = 0;
+	if (n == 0)
+	{
+		size++;
+		return (size);
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		size++;
+	}
+	return (size);
+}
+
+char			*ft_itoa(int n)
+{
+	char		*str;
+	size_t		size;
+	long int	conv;
+	int			neg;
+
+	neg = 0;
+	size = 0;
+	conv = (long int)n;
+	if (n < 0)
+	{
+		conv = -conv;
+		size = (size_t)get_size(conv);
+		size++;
+		neg = 1;
+	}
+	else
+		size = get_size(conv);
+	if (!(str = ft_memalloc(size + 1)))
+		return (NULL);
+	str = ft_makestr(str, size, conv, neg);
+	return (str);
+}
---
+++ ft_lstmap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alfiumic <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/19 18:52:59 by alfiumic          #+#    #+#             */
+/*   Updated: 2018/11/20 16:22:05 by alfiumic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list *new;
+	t_list *list;
+
+	if (!lst)
+		return (NULL);
+	list = f(lst);
+	new = list;
+	while (lst->next)
+	{
+		lst = lst->next;
+		if (!(list->next = f(lst)))
+		{
+			free(list->next);
+			return (NULL);
+		}
+		list = list->next;
+	}
+	return (new);
+}
